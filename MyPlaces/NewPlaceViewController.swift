e868186a120e9ed82fcc6068b0f66f64f536317e
artemiosdev <flyboroda@gmail.com>
2022-12-04 23:47:15 +0300

    Добавил возможность выбора изображения при тапе в первой ячейке

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -8,19 +8,32 @@
 import UIKit
 
 class NewPlaceViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
-            //доб появление меню для выбора фото new place
+            //добавим меню для выбора фото new place
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(sourse: .camera)
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(sourse: .photoLibrary)
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            actionSheet.addAction(camera)
+            actionSheet.addAction(cancel)
+            actionSheet.addAction(photo)
+            
+            present(actionSheet, animated: true)
         } else {
             view.endEditing(true)
         }
     }
-
-
+    
+    
 }
 // MARK: - Table view delegate
 extension NewPlaceViewController: UITextFieldDelegate {
@@ -30,4 +43,17 @@ extension NewPlaceViewController: UITextFieldDelegate {
         return true
     }
     // скрываем клавиатуру по тапу на ячейку
+}
+
+// MARK: - Work with image
+extension NewPlaceViewController {
+    func chooseImagePicker(sourse: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourse) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = sourse
+            present(imagePicker, animated: true)
+            
+        }
+    }
 }
