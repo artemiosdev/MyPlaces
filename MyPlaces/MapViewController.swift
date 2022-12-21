21040682a1a658a800de5f9c796805502d14b0aa
artemiosdev <flyboroda@gmail.com>
2022-12-21 15:13:29 +0300

    Добавил и оформил иконки для Карты по локации объекта, и подготовил view

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Artem Androsenko on 21.12.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
